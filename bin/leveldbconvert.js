839bbd9dab47ebcea5419a64252d8d37fad92d4e
Andrija Petrovic <andrija.hers@gmail.com>
2016-11-24 12:24:45 +0100

    allex-leveldb-convert functional again

--- bin/leveldbconvert.js
+++ bin/leveldbconvert.js
@@ -1,6 +1,42 @@
 #!/usr/bin/env node
 
 var child_process = require('child_process'),
-  path = require('path');
+  path = require('path'),
+  originaldbname = process.argv[2],
+  convertscript = process.argv[3],
+  scriptpath;
 
-child_process.exec('allexrun '+ path.join(__dirname, '..', 'binscripts', 'leveldbconvert.js '+process.argv[2]+' '+process.argv[3]), console.log.bind(console, 'done'));
+/*
+convertscript should module.exports = {
+  inOptionsCreator: function (execlib, bufferlib, leveldblib) {...},
+  outOptionsCreator: function (execlib, bufferlib, leveldblib) {...},
+  convertor: function (keyvalue) { return {key: ..., value: ...} }
+};
+
+instead of convertor: function...
+there may be createConvertor(execlib, bufferlib, leveldblib) {...}
+
+*/
+
+function printUsage() {
+  console.log('Usage: allex-leveldb-convert dbfilename convertscriptname');
+  console.log('');
+  console.log('convertscript should module.exports = {');
+  console.log('  inOptionsCreator: function (execlib, bufferlib, leveldblib) {...},');
+  console.log('  outOptionsCreator: function (execlib, bufferlib, leveldblib) {...},');
+  console.log('  convertor: function (keyvalue) { return {key: ..., value: ...} }');
+  console.log('};');
+  console.log('');
+  console.log('instead of convertor: function...');
+  console.log('there may be createConvertor(execlib, bufferlib, leveldblib) {...}');
+  process.exit(1);
+}
+
+if (!originaldbname) {
+  return printUsage();
+}
+if (!convertscript) {
+  return printUsage();
+}
+scriptpath = path.relative(process.cwd(), path.join(__dirname, '..', 'binscripts', 'leveldbconvert.js '+originaldbname+' '+convertscript));
+child_process.exec('allexrun '+ scriptpath, console.log.bind(console, 'done'));
