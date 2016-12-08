868e7ef8e8b4589c8fc16516ee31bd156ac83bac
Andrija Petrovic <andrija.hers@gmail.com>
2016-12-08 12:11:22 +0100

    + the test script

---
+++ test/testduplicatedb.js
@@ -0,0 +1,23 @@
+function eventuallyClose (db) {
+  setTimeout(db.destroy.bind(db), 5000);
+}
+
+function startdb (execlib, leveldblib) {
+  var d = execlib.lib.q.defer();
+  d.promise.then(eventuallyClose);
+  new leveldblib.LevelDBHandler({
+    dbname: 'test/bla/test.db',
+    starteddefer: d
+  });
+}
+
+function go (execlib, leveldblib) {
+  startdb(execlib, leveldblib);
+  startdb(execlib, leveldblib);
+}
+
+function main (execlib) {
+  execlib.loadDependencies('client', ['allex:leveldb:lib'], go.bind(null, execlib));
+}
+
+module.exports = main;
