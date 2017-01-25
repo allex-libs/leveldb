71ad56310617e0bd093ae3c5f6eba3e3a412b8e0
Andrija Petrovic <andrija.hers@gmail.com>
2017-01-25 12:16:12 +0000

    And the int64codeccreator

---
+++ codecs/int64codeccreator.js
@@ -0,0 +1,24 @@
+function createInt64Codec(execlib) {
+  return {
+    encode: function(num) {
+      var ret = new Buffer(8),
+        hi = ~~(num / 0x100000000),
+        lo = num % 0x100000000;
+    //console.log(item, '=> lo', lo, 'hi', hi);
+      ret.writeUInt32BE(lo, 0);
+      ret.writeUInt32BE(hi, 4);
+      return ret;
+    },
+    decode: function (buff) {
+      var ret = 0,
+        lo = buff.readUInt32BE(0),
+        hi = buff.readUInt32BE(4);
+      ret += (lo);
+      ret += ( hi * 0x100000000);
+      return ret;
+    },
+    buffer: true,
+    type: 'int64'
+  };
+}
+module.exports = createInt64Codec;
